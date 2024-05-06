ab2ebb5608402add89e4bf73ecb58b30ffcdf221
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-05-06 11:36:06 +0530

    navbar color changed

--- frontend/src/components/header/navbar.jsx
+++ frontend/src/components/header/navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="relative bg-green-500 shadow-lg">
-      <div className="container px-6 py-4 mx-auto">
+    <nav className="relative bg-black shadow-lg">
+      <div className="container px-5 py-4 mx-auto">
         <div className="lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
             <a href="#">
@@ -17,7 +17,7 @@ const Navbar = () => {
             </a>
             <div className="flex lg:hidden">
               <button onClick={toggleMenu} type="button" className="text-gray-100 hover:text-gray-200 focus:outline-none" aria-label="toggle menu">
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="1 0 24 24" stroke="currentColor" strokeWidth="2">
                   {isOpen ? (
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                   ) : (
