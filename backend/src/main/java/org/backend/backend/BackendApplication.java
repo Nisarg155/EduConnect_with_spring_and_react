6fc8024a4c80024719b57cfaa969317ba2b5b52a
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-06-08 19:16:29 +0530

    firebase config removed v.3

--- backend/src/main/java/org/backend/backend/BackendApplication.java
+++ backend/src/main/java/org/backend/backend/BackendApplication.java
@@ -1,24 +1,14 @@
 package org.backend.backend;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Objects;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	@Autowired
-	private Environment env;
+
 
 
 	public static void main(String[] args) throws IOException {
