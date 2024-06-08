46d9c5d27dac1bc6ebbe35abc172c2b8e48ebc0b
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-06-08 19:09:38 +0530

    firebase config removed v.2

--- backend/src/main/java/org/backend/backend/repositories/firebase.java
+++ backend/src/main/java/org/backend/backend/repositories/firebase.java
@@ -1,20 +1,20 @@
-package org.backend.backend.repositories;
-
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.cloud.FirestoreClient;
-import org.backend.backend.model.users;
-import org.springframework.stereotype.Repository;
-
-import java.util.concurrent.ExecutionException;
-
-@Repository
-public class firebase
-{
-   public   String create(users user) throws ExecutionException, InterruptedException {
-        Firestore fsdb = FirestoreClient.getFirestore();
-        ApiFuture<WriteResult> apiFuture = fsdb.collection("users").document(user.getUid()).set(user);
-        return apiFuture.get().getUpdateTime().toString();
-    }
-}
+//package org.backend.backend.repositories;
+//
+//import com.google.api.core.ApiFuture;
+//import com.google.cloud.firestore.Firestore;
+//import com.google.cloud.firestore.WriteResult;
+//import com.google.firebase.cloud.FirestoreClient;
+//import org.backend.backend.model.users;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.concurrent.ExecutionException;
+//
+//@Repository
+//public class firebase
+//{
+//   public   String create(users user) throws ExecutionException, InterruptedException {
+//        Firestore fsdb = FirestoreClient.getFirestore();
+//        ApiFuture<WriteResult> apiFuture = fsdb.collection("users").document(user.getUid()).set(user);
+//        return apiFuture.get().getUpdateTime().toString();
+//    }
+//}
