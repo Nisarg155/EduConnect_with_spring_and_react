edbc9ee5d006c42cfb499d1bc2995490198d5759
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-06-15 19:19:40 +0530

    refectored the code

--- backend/src/main/java/org/backend/backend/controllers/controller.java
+++ backend/src/main/java/org/backend/backend/controllers/controller.java
@@ -1,7 +1,6 @@
 package org.backend.backend.controllers;
 
 
-
 import org.backend.backend.model.*;
 import org.backend.backend.repositories.*;
 import org.backend.backend.repositories.Assignment;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 @RestController
@@ -24,6 +22,7 @@ public class controller {
     private final Materials_repo materials_repo;
     private final Assignment assignment;
     private final StudentClass studentClass;
+
     @Autowired
     controller(Student_repo studentRepo, Teacher_repo teacher_repo, Class_repo classRepo, Materials_repo materialsRepo, Assignment assignment, StudentClass studentClass) {
         this.studentRepo = studentRepo;
@@ -119,8 +118,7 @@ public class controller {
     @PostMapping("/material/upload")
     public ResponseEntity<List<Materials>> upload_material(@RequestBody Map<String, Object> data) {
         String code = generateRandomCode();
-        while(materials_repo.findByCode(code) != null)
-        {
+        while (materials_repo.findByCode(code) != null) {
             code = generateRandomCode();
         }
         String class_code = data.get("code").toString();
@@ -128,15 +126,14 @@ public class controller {
         String description = data.get("description").toString();
         List<String> urls = (List<String>) data.get("urls");
         List<String> file_names = (List<String>) data.get("file_names");
-        Materials materials = new Materials(title, description, urls, file_names,class_code,code);
+        Materials materials = new Materials(title, description, urls, file_names, class_code, code);
         materials_repo.save(materials);
         List<Materials> materialsList = materials_repo.findByClass_id(class_code);
         return ResponseEntity.ok(materialsList);
     }
 
     @GetMapping("materials/{code}")
-    public ResponseEntity<List<Materials>> get_materials(@PathVariable String code)
-    {
+    public ResponseEntity<List<Materials>> get_materials(@PathVariable String code) {
         List<Materials> list = materials_repo.findByClass_id(code);
         return ResponseEntity.ok(list);
     }
@@ -150,12 +147,11 @@ public class controller {
     }
 
     @PostMapping("Assignment/{class__code}")
-    public ResponseEntity<List<org.backend.backend.model.Assignment>> add_assignment(@PathVariable String class__code, @RequestBody Map<String,Object> data) {
+    public ResponseEntity<List<org.backend.backend.model.Assignment>> add_assignment(@PathVariable String class__code, @RequestBody Map<String, Object> data) {
 
         String code = generateRandomCode();
 
-        while(assignment.findByUniqueCode(code) != null)
-        {
+        while (assignment.findByUniqueCode(code) != null) {
             code = generateRandomCode();
         }
         String title = data.get("title").toString();
@@ -165,7 +161,7 @@ public class controller {
         System.out.println(data.get("sub_date").toString());
         boolean latesub = (boolean) data.get("late_sub");
 
-        org.backend.backend.model.Assignment assignment1 = new org.backend.backend.model.Assignment(title,description,lastdate,latesub,code, class__code);
+        org.backend.backend.model.Assignment assignment1 = new org.backend.backend.model.Assignment(title, description, lastdate, latesub, code, class__code);
         assignment.save(assignment1);
 
         return ResponseEntity.ok(assignment.findByClassCode(class__code));
@@ -176,15 +172,13 @@ public class controller {
     ResponseEntity<List<Classes>> join_class(@PathVariable String uid, @PathVariable String code) {
 
         Optional<Classes> optionalClasses = class_repo.findById(code);
-        if(optionalClasses.isEmpty()) return ResponseEntity.ok(null);
+        if (optionalClasses.isEmpty()) return ResponseEntity.ok(null);
 
         Optional<org.backend.backend.model.Student_Class> student_class = studentClass.findById(uid);
-        if(student_class.isPresent())
-        {
+        if (student_class.isPresent()) {
             List<String> codes = student_class.get().getCodes();
 
-            if(codes.contains(code))
-            {
+            if (codes.contains(code)) {
                 return ResponseEntity.ok(null);
             }
             codes.add(code);
@@ -192,18 +186,16 @@ public class controller {
             studentClass.save(student_class.get());
 
             List<Classes> classesList = new ArrayList<>();
-            Optional<Classes> optionalClasses1 ;
-            for(String code1 : codes)
-            {
+            Optional<Classes> optionalClasses1;
+            for (String code1 : codes) {
                 optionalClasses1 = class_repo.findById(code1);
                 optionalClasses1.ifPresent(classesList::add);
             }
             return ResponseEntity.ok(classesList);
-        }
-        else{
+        } else {
             List<String> codes = new ArrayList<>();
             codes.add(code);
-            org.backend.backend.model.Student_Class student_class1 = new org.backend.backend.model.Student_Class(uid,codes);
+            org.backend.backend.model.Student_Class student_class1 = new org.backend.backend.model.Student_Class(uid, codes);
             studentClass.save(student_class1);
             Optional<Classes> classes = class_repo.findById(code);
             return classes.map(value -> ResponseEntity.ok(Collections.singletonList(value))).orElseGet(() -> ResponseEntity.ok(null));
@@ -213,22 +205,19 @@ public class controller {
     @DeleteMapping("/RemoveClass/{uid}/{code}")
     ResponseEntity<List<Classes>> class_remove(@PathVariable String uid, @PathVariable String code) {
         Optional<Student_Class> student_class = studentClass.findById(uid);
-        if(student_class.isPresent())
-        {
+        if (student_class.isPresent()) {
             List<String> codes = student_class.get().getCodes();
             codes.remove(code);
             student_class.get().setCodes(codes);
             studentClass.save(student_class.get());
             List<Classes> classesList = new ArrayList<>();
-            Optional<Classes> optionalClasses1 ;
-            for(String code1 : codes)
-            {
+            Optional<Classes> optionalClasses1;
+            for (String code1 : codes) {
                 optionalClasses1 = class_repo.findById(code1);
                 optionalClasses1.ifPresent(classesList::add);
             }
             return ResponseEntity.ok(classesList);
-        }
-        else{
+        } else {
             return ResponseEntity.ok(null);
         }
     }
