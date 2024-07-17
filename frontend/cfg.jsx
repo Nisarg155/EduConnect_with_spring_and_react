15ba999eb094171fb76c589b9a09fd4738794ac7
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-07-17 17:58:07 +0530

    for vercel deployment

---
+++ frontend/cfg.jsx
@@ -0,0 +1,14 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDoKKxEn-IChCtdV8BuoohMf7TMZyZXZxk",
+    authDomain: "educonnect-6789e.firebaseapp.com",
+    databaseURL: "https://educonnect-6789e-default-rtdb.firebaseio.com",
+    projectId: "educonnect-6789e",
+    storageBucket: "educonnect-6789e.appspot.com",
+    messagingSenderId: "422805237793",
+    appId: "1:422805237793:web:7fcf5afa6d241f935b3360",
+    measurementId: "G-XHXM785F62"
+};
+
+export default firebaseConfig;
