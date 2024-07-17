e68a9d86f4e3d76a7d62f836422d9f2390104562
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-07-17 22:16:56 +0530

    updated backend application

--- backend/src/main/java/org/backend/backend/BackendApplication.java
+++ backend/src/main/java/org/backend/backend/BackendApplication.java
@@ -1,18 +1,29 @@
 package org.backend.backend;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
 
-
-
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://edu-connect-with-spring-and-react.vercel.app/")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
