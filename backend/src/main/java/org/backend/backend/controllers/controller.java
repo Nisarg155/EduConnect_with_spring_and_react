14fb4d3bf4c7cb48b1d9c1abf0fbc4e4c589c827
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-07-17 15:38:21 +0530

    updated cors

--- backend/src/main/java/org/backend/backend/controllers/controller.java
+++ backend/src/main/java/org/backend/backend/controllers/controller.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 public class controller {
 
 
